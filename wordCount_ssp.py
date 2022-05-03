da3e54a54f088edc534284d0f15aec48d61e37a8
Wagner M Cunha <wagnermc506@gmail.com>
2022-05-03 00:32:39 -0300

    atualizando word_count com a api ssp

--- wordCount_ssp.py
+++ wordCount_ssp.py
@@ -1,26 +1,41 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, split, col, length
+import sys
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import explode, split, col, length, substring
 
 spark = SparkSession.builder.master("spark://192.168.8.5:7077").appName("WordCountKafka").getOrCreate()
 
 spark.sparkContext.setLogLevel("WARN")
 
-# lines = spark.readStream.format("kafka")\
-#             .option("kafka.bootstrap.servers", "192.168.8.5:9092")\
-#             .option("subscribe", "words")\
-#             .load()
-lines = spark.readStream.format("socket").option("host", "192.168.8.5").option("port", 9999).load()
+def basic_word_count(lines: DataFrame):
+    words = lines.select(explode(split(lines.value, " ")).alias("word"))
+    word_counts = words.groupBy("word").count()
+    query = word_counts.writeStream.outputMode("complete").format("console").start()
+    query.awaitTermination()
 
-words = lines.select(explode(split(lines.value, " ")).alias("word"))
-wordCounts = words.groupBy("word").count()
+def word_count_for_letters(lines: DataFrame):
+    words = lines.select(explode(split(lines.value, " ")).alias("word"))
+    first_letters = words.select(col('word').substr(1, 1).alias("letter")).filter("letter in ('S', 'P', 'R')").groupBy("letter").count()
+    query = first_letters.writeStream.outputMode("complete").format("console").start()
+    query.awaitTermination()
 
-words_with_SPR = words.filter(length(words['word']) > 3).groupBy("word").count()
+def word_count_by_length(lines: DataFrame):
+    words = lines.select(explode(split(lines.value, " ")).alias("word"))
+    lengths = words.select(length("word").alias('len')).groupBy('len').count()
+    query = lengths.writeStream.outputMode("complete").format("console").start()
+    query.awaitTermination()
 
-query = wordCounts.writeStream.outputMode("complete").format("console").start()
 
-query2 = words_with_SPR.writeStream.outputMode("complete").format("console").start()
+if __name__ == '__main__':
 
-# query = wordCounts.writeStream.format("kafka").option("checkpointLocation", "/tmp/checkpoint").option("kafka.bootstrap.servers", "192.168.8.5:9092").option("topic", "text_topic").start()
+    if len(sys.argv) < 4:
+        print(f"uso: {sys.argv[0]} <host> <port> [basic|letter|len]")
+        exit(0)
 
-query.awaitTermination()
-query2.awaitTermination()
+    lines = spark.readStream.format("socket").option("host", "192.168.8.5").option("port", 9999).load()
+
+    if sys.argv[3] == 'basic':
+        basic_word_count(lines)
+    elif sys.argv[3] == 'letter':
+        word_count_for_letters(lines)
+    elif sys.argv[3] == 'len':
+        word_count_by_length(lines)
